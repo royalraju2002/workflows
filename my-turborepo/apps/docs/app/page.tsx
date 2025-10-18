8da32dcf998c3e6381b8898f978fabae3848bc4d
Raju Kumar <rajukumar706129@gmail.com>
2025-10-18 21:57:30 +0000

    init

--- my-turborepo/apps/docs/app/page.tsx
+++ my-turborepo/apps/docs/app/page.tsx
@@ -31,12 +31,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>apps/docs/app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1>Hello, Wordl</h1>
 
         <div className={styles.ctas}>
           <a
